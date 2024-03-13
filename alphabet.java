b0492ddf3849a5f5836b41be6e4c24a0bef12d37
kolekarom <160025078+kolekarom@users.noreply.github.com>
2024-03-13 11:19:44 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class alphabet {
+    public static void main(String[] args){
+        int i;
+//        for (i=65; i<=90;i++){
+//            char b= (char)i;
+//            System.out.println(b);
+//        }
+        for(i=25;i>=0;i--){
+            int j=65;
+            char b=(char)(i+j);
+            System.out.println(b);
+        }
+    }
+}
