09a9cc2321173fafbc8c9c1d3390e90cffcea4e3
kolekarom <160025078+kolekarom@users.noreply.github.com>
2024-03-16 09:15:40 +0530

    Add files via upload

---
+++ math_functions.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class math_functions {
+    public static void main(String[] args){
+        System.out.println("absulate : "+Math.abs(-3));
+        System.out.println("maximun value : "+Math.max(Math.max(2,5),6));
+        System.out.println("minimum value : "+Math.min(Math.min(2,5),6));
+        System.out.println("cbrt : "+Math.cbrt(4.63));
+        System.out.println("sqrt : "+Math.sqrt(4.63));
+        System.out.println("absulate : "+Math.abs(-3));
+        System.out.println("ceil: "+Math.ceil(4.49));
+
+    }
+}
