8b188827e2e7cd3862edbd5ca79c787d89c67c55
kolekarom <160025078+kolekarom@users.noreply.github.com>
2024-03-19 10:44:21 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+    public class second {
+        class demo {
+            int a;
+            String b = "om";
+    
+            void show() {
+                System.out.println(a + " " + b);
+            }
+        }
+            class test{
+                public void main(String[] args){
+                    demo ref = new demo();
+                    ref.show();;
+    
+    
+    
+                }
+            }
+    
+    }
